52f934369e77466a39b3de680f8e2ef3f327b6c0
JordanAndrs <113586182+JordanAndrs@users.noreply.github.com>
2022-09-29 10:22:26 +0200

    Add files via upload

---
+++ Depuracion/PruebasDepuracion/PruebasDepuracion/PruebasDepuracion/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PruebasDepuracion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 3;
+
+            Console.Write("Introduce un número: ");
+            a = int.Parse(Console.ReadLine());
+
+            Console.Write("Introduce otro número: ");
+            b = int.Parse(Console.ReadLine());
+
+            Console.Write("El resultado es : " + ( a + b ));
+           
+        }
+    }
+}
