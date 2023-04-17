c0d945eabf42cadab3bbb1dd2544314d6595368c
JordanAndrs <113586182+JordanAndrs@users.noreply.github.com>
2023-04-18 01:08:59 +0200

    Add files via upload

---
+++ Proyectos C#/TrabajosDeApoyo/TrabajosDeApoyo/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrabajosDeApoyo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ej6();
+            Console.ReadLine();
+        }
+        static void Ej1()
+        {
+            /* Mostrar todos los números del 0 al 100 */
+
+            for (int i = 0; i < 101; i++)
+            {
+                Console.WriteLine("Numero: " + i.ToString());
+            }
+        }
+        static void Ej2()
+        {
+            /* Mostrar todos los números del 0 al 100 y decir si son par o impar */
+
+            for (int i = 0; i < 101; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine("El numero " + i.ToString() + " es par");
+                }
+                else
+                {
+                    Console.WriteLine("El número " + i.ToString() + " es impar");
+                }
+            }
+        }
+        static void Ej3()
+        {
+            //Función que pida una temperatura en Celsius y calcule y devuelva Fahrenheit y Kelvin
+
+            Console.Write("Introduzca la temperatura en grados Celsius: ");
+            Double Celsius = Convert.ToDouble(Console.ReadLine());
+
+            Double Fahrenheit = Celsius * 9 / 5 + 32;
+            Console.WriteLine("La temperatura en Fahrenheit es: " + Fahrenheit);
+            
+            Double Kelvin = Celsius + 273.15;
+            Console.WriteLine("La temperatura en Kelvin es: " + Kelvin);
+        }
+        static void Ej4()
+        {
+            /* Función que lee un número que corresponde al radio de una circunferencia y calcule e imprima la
+            longitud de la misma y el área del círculo correspondiente.*/
+
+            Console.Write("Introduzca el radio: ");
+            Double radio = Convert.ToDouble(Console.ReadLine());
+
+            Double Longitud = ((radio * 2) * 3.1415);
+            Console.WriteLine("La longitud de la circunferencia es: " + (Longitud));
+
+            Double área = Math.Pow(radio, 2) * 3.1415;
+            Console.WriteLine("El área de la circunferencia es: " + (área));           
+        }
+        static void Ej5()
+        {
+            /* Función que lee dos números y calcule e imprima la suma, resta, producto y división */
+
+            Console.Write("Introduzca un número: ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Introduzca otro número: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            int suma = num1 + num2;
+            Console.WriteLine("La suma de los dos numeros es " + suma);
+
+            int resta = num1 - num2;
+            Console.WriteLine("La resta de los dos numeros es " + resta);
+
+            int producto = num1 * num2;
+            Console.WriteLine("El producto de los dos números es " + producto);
+
+            int division = num1 / num2;
+            Console.WriteLine("La división entre los dos números es " + division);
+        }
+        static void Ej6()
+        {
+            /* Implementar una función que convierta a euros una determinada cantidad en pesetas que se recibirán como parámetro de 
+               entrada(1 euro = 166.386 pesetas)*/
+
+            Console.Write("Introduzca el número de pesetas a convertir en euros: ");
+
+            double dinero = Convert.ToDouble(Console.ReadLine());
+
+            double pesetas = 166.386;
+
+            double euros = dinero / pesetas;
+
+            Console.WriteLine("Las pesetas introducidas son " + euros + " euros");
+        }
+    }
+}
